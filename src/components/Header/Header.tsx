2c5aaa996795bfbad8ebdd8b5bee41028261381e
paulo-fs <jr.paulo87@gmail.com>
2023-06-10 16:49:11 -0300

    refactor: set fixed position to header menu and add opacity

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,9 @@ import { HeaderBasket } from '../HeaderBasket'
 
 export function Header() {
   return (
-    <menu className="backdrop-blur-md bg-white flex justify-between items-center px-28 py-8">
+    <menu 
+      className="backdrop-blur-md bg-white/60 flex justify-between items-center px-28 py-8 fixed top-0 right-0 left-0 border-b border-green-100"
+    >
       <Image src={logo} alt='logo Verde Vida' />
 
       <nav className='flex gap-5'>
