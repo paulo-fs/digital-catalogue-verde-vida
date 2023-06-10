c96a6bc680f227ed17ce3050089a1c3bed79e8de
paulo-fs <jr.paulo87@gmail.com>
2023-06-10 15:54:51 -0300

    chore: add eslint configs and prettier for organize tailwind css

--- src/app/Home/Footer/index.tsx
+++ src/app/Home/Footer/index.tsx
@@ -1,33 +1,33 @@
-import { CustomLink } from "@/components";
+import { CustomLink } from '@/components'
 
 export function Footer() {
-   return (
-      <footer className="bg-green-300 bg-opacity-50 px-28 mt-20 flex flex-col items-center gap-4 pb-8 pt-12">
+  return (
+    <footer className="bg-green-300 bg-opacity-50 px-28 mt-20 flex flex-col items-center gap-4 pb-8 pt-12">
 
-         <div className="flex gap-36">
-            <div className="flex flex-col gap-4">
-               <h3 className="font-title font-bold text-h4">A Verde Vida</h3>
-               <CustomLink href='/' text="Sobre nós" />
-               <CustomLink href='/' text="Nossos contatos" />
-               <CustomLink href='/' text="Certificações" />
-            </div>
-            <div className="flex flex-col gap-4">
-               <h3 className="font-title font-bold text-h4">Nosso catálogo</h3>
-               <CustomLink href='/' text='Categorias' />
-               <CustomLink href='/' text='Minhas cestinha' />
-               <CustomLink href='/' text='Como funciona' />
-            </div>
-            <div className="flex flex-col gap-4">
-               <h3 className="font-title font-bold text-h4">Área científica</h3>
-               <CustomLink href='' text='Artigos científicos sobre nossos ativos' />
-            </div>
-         </div>
+      <div className="flex gap-36">
+        <div className="flex flex-col gap-4">
+          <h3 className="font-title font-bold text-h4">A Verde Vida</h3>
+          <CustomLink href='/' text="Sobre nós" />
+          <CustomLink href='/' text="Nossos contatos" />
+          <CustomLink href='/' text="Certificações" />
+        </div>
+        <div className="flex flex-col gap-4">
+          <h3 className="font-title font-bold text-h4">Nosso catálogo</h3>
+          <CustomLink href='/' text='Categorias' />
+          <CustomLink href='/' text='Minhas cestinha' />
+          <CustomLink href='/' text='Como funciona' />
+        </div>
+        <div className="flex flex-col gap-4">
+          <h3 className="font-title font-bold text-h4">Área científica</h3>
+          <CustomLink href='' text='Artigos científicos sobre nossos ativos' />
+        </div>
+      </div>
 
 
-         <div className="w-full border-t border-green-300 flex gap-48 justify-center mt-8 pt-8">
-            <p>Todos os direitos reservados.</p>
-            <p>Desenvolvido por: Paulo Souza</p>
-         </div>
-      </footer>
-   )
+      <div className="w-full border-t border-green-300 flex gap-48 justify-center mt-8 pt-8">
+        <p>Todos os direitos reservados.</p>
+        <p>Desenvolvido por: Paulo Souza</p>
+      </div>
+    </footer>
+  )
 }
--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,17 +1,17 @@
-import { Footer } from "./Footer"
-import { Hero } from "./Hero"
-import { HomeCategories } from "./HomeCategories"
-import { HowItWorks } from "./HowItWorks"
+import { Footer } from './Footer'
+import { Hero } from './Hero'
+import { HomeCategories } from './HomeCategories'
+import { HowItWorks } from './HowItWorks'
 
 export default function Home() {
-   return (
-      <>
-         <div className="px-28 font-body text-gray-900 flex flex-col gap-14">
-            <Hero />
-            <HowItWorks />
-            <HomeCategories />
-         </div>
-         <Footer />
-      </>
-   )
+  return (
+    <>
+      <div className="px-28 font-body text-gray-900 flex flex-col gap-14">
+        <Hero />
+        <HowItWorks />
+        <HomeCategories />
+      </div>
+      <Footer />
+    </>
+  )
 }
