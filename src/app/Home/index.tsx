e9ad285152635975633b34c55388d75acdd2c375
paulo-fs <jr.paulo87@gmail.com>
2023-06-11 20:33:58 -0300

    fix: add max width

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -4,7 +4,7 @@ import { HowItWorks } from './HowItWorks'
 
 export default function Home() {
   return (
-    <div className="px-28 font-body text-gray-900 flex flex-col gap-14">
+    <div className="px-28 mt-36 font-body text-gray-900 flex flex-col gap-14 max-w-7xl mx-auto">
       <Hero />
       <HowItWorks />
       <HomeCategories />
