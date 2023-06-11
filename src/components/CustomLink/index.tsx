852753bf7783a9004d30dd7f79a6c4bec1faf5fc
paulo-fs <jr.paulo87@gmail.com>
2023-06-10 21:16:34 -0300

    fix: add hover effect

--- src/components/CustomLink/index.tsx
+++ src/components/CustomLink/index.tsx
@@ -6,7 +6,7 @@ interface CustomLinkProps extends LinkProps {
 
 export function CustomLink({ text, ...props }: CustomLinkProps) {
   return (
-    <Link {...props} className="text-b3 font-body text-green-500 underline">
+    <Link {...props} className="text-b3 font-body text-green-500 hover:text-green-300 transition disabled:text-gray-700 underline">
       {text}
     </Link>
   )
