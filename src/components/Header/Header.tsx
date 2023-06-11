2143aa3221b59c49c05d41f9651e0609bad97230
paulo-fs <jr.paulo87@gmail.com>
2023-06-10 21:16:44 -0300

    fix: add z-index

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,8 @@ import { HeaderBasket } from '../HeaderBasket'
 
 export function Header() {
   return (
-    <menu 
-      className="backdrop-blur-md bg-white/60 flex justify-between items-center px-28 py-8 fixed top-0 right-0 left-0 border-b border-green-100"
+    <menu
+      className="backdrop-blur-md bg-white/60 flex justify-between items-center px-28 py-8 fixed top-0 right-0 left-0 z-10 border-b border-green-100"
     >
       <Image src={logo} alt='logo Verde Vida' />
 
