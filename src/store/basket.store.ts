fed5fc62402378d25c903f47b7cb0966cd791c19
paulo-fs <jr.paulo87@gmail.com>
2023-07-10 11:41:48 -0300

    feat: add basket managment state

---
+++ src/store/basket.store.ts
@@ -0,0 +1,80 @@
+import { create } from 'zustand'
+import { IBasketItem } from '@/dataTypes/basket.dto'
+
+const dummyProductList = [
+  {
+    id: '1',
+    name: 'vital drops 1',
+    price: 20,
+    amount: 1
+  },
+  {
+    id: '2',
+    name: 'vital drops 2',
+    price: 20.5,
+    amount: 1
+  },
+  {
+    id: '3',
+    name: 'vital drops 3',
+    price: 10,
+    amount: 1
+  },
+]
+
+interface BasketProps {
+  basketContent: IBasketItem[] | null
+  basketTotal: number
+
+  setTotal: () => void
+  increaseAmount: (id: string) => void
+  decreaseAmount: (id: string) => void
+}
+
+const initialState = {
+  basketContent: dummyProductList,
+  basketTotal: 0
+}
+
+export const basketStore = create<BasketProps>((set, get) => ({
+  ...initialState,
+
+  setTotal: () => {
+    const { basketContent } = get()
+    const result = basketContent?.reduce((total, curItem) => (
+      total + (curItem.price * curItem.amount)
+    ), 0)
+    set({ basketTotal: result })
+  },
+
+  increaseAmount: (id) => {
+    const { basketContent, setTotal } = get()
+    console.log('increase', id)
+
+    set({
+      basketContent: basketContent?.map((item) => {
+        if (item.id === id) {
+          return {...item, amount: item.amount + 1}
+        }
+        return item
+      })
+    })
+    setTotal()
+  },
+
+  decreaseAmount: (id) => {
+    const { basketContent, setTotal } = get()
+    console.log('decrease', id)
+
+    set({
+      basketContent: basketContent?.map((item) => {
+        if (item.id === id) {
+          const resultAmount = item.amount > 1 ? item.amount - 1 : item.amount
+          return {...item, amount: resultAmount}
+        }
+        return item
+      })
+    })
+    setTotal()
+  },
+}))
