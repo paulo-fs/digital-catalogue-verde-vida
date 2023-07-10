1530448ee008b36613cff1db6885bfff64e0d921
paulo-fs <jr.paulo87@gmail.com>
2023-07-10 14:47:00 -0300

    chore: add fake data

---
+++ src/store/fakeData.ts
@@ -0,0 +1,73 @@
+import { IProduct } from '@/dataTypes/product.dto'
+
+export const fakeProductsOnBasket = [
+  {
+    id: '1',
+    name: 'vital drops 1',
+    price: 20,
+    amount: 1
+  },
+  {
+    id: '2',
+    name: 'vital drops 2',
+    price: 20.5,
+    amount: 1
+  },
+  {
+    id: '3',
+    name: 'vital drops 3',
+    price: 10,
+    amount: 1
+  },
+]
+
+export const fakeProductList: IProduct[] = [
+  {
+    id: '4',
+    name: 'produto 4',
+    price: 40,
+    category: 'health',
+    available: true,
+    image: '/'
+  },
+  {
+    id: '5',
+    name: 'produto 5',
+    price: 50,
+    category: 'health',
+    available: true,
+    image: '/'
+  },
+  {
+    id: '6',
+    name: 'produto 6',
+    price: 60,
+    category: 'health',
+    available: true,
+    image: '/'
+  },
+  {
+    id: '7',
+    name: 'produto 7',
+    price: 70,
+    category: 'health',
+    available: true,
+    image: '/'
+  },
+  {
+    id: '8',
+    name: 'produto 8',
+    price: 80,
+    category: 'health',
+    available: true,
+    image: '/'
+  },
+  {
+    id: '9',
+    name: 'produto 9',
+    price: 90,
+    category: 'health',
+    available: true,
+    image: '/'
+  },
+]
