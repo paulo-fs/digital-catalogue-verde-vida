6c34c1bc7fca4c06df15118f9e906b0e85fdc0ba
paulo-fs <jr.paulo87@gmail.com>
2023-07-10 14:07:46 -0300

    feat: implement dynamic total on head

--- src/components/HeaderBasket/index.tsx
+++ src/components/HeaderBasket/index.tsx
@@ -1,10 +1,20 @@
+'use client'
 import Image from 'next/image'
 import smallBagIcon from '../../assets/svg/smallBagIcon.svg'
+import { basketStore } from '@/store/basket.store'
+import { formatPrice } from '@/helpers/formatPrice'
+import { useEffect } from 'react'
 
 export function HeaderBasket() {
+  const { basketTotal, setTotal } = basketStore()
+
+  useEffect(() => {
+    setTotal()
+  }, [setTotal])
+
   return (
     <div className='flex gap-4 items-center'>
-      <span className='font-body text-b2 text-green-500'>R$30,00</span>
+      <span className='font-body text-b2 text-green-500'>R${formatPrice(basketTotal)}</span>
       <Image src={smallBagIcon} alt='ícone de uma sacola de compras' />
     </div>
   )
