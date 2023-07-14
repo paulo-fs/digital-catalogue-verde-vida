e79d3fd0b8bd3844cfe9466c8c1929c2c42121a1
paulo-fs <jr.paulo87@gmail.com>
2023-07-14 14:43:22 -0300

    feat: add basic crud operations to api on category

---
+++ src/app/api/category/[id]/route.ts
@@ -0,0 +1,57 @@
+import { Slug } from '@/helper/slugNormalizer'
+import database from '@/lib/database'
+import Category from '@/models/category'
+import { NextResponse, NextRequest } from 'next/server'
+
+database.connect()
+
+export const config = {
+  api: {
+    bodyParser: false,
+  }
+}
+
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+  const body = await req.json()
+  const { id } = params
+
+  try {
+    const category = await Category.findById(id)
+    const newSlug = body.name ? Slug.createFromText(body.name).value : null
+
+    if (!category) return NextResponse.json({ error: 'Category not found' }, { status: 404 })
+
+    category.name = body.name || category.name
+    category.icon = body.icon || category.icon
+    category.slug = newSlug || (category.slug)
+    category.save()
+
+    return NextResponse.json({category})
+  } catch (err) {
+    return NextResponse.json({ error: 'Could not update the category'})
+  }
+}
+
+export async function DELETE(_req: Request, { params }: { params: { id: string } }) {
+  const { id } = params
+
+  try {
+    await Category.findByIdAndDelete(id)
+    return NextResponse.json({ message: 'Category deleted successfully' })
+  } catch (err) {
+    console.log(err)
+    return NextResponse.json({ error: 'Category not found' }, { status: 404 })
+  }
+}
+
+export async function GET(_req: Request, { params }: { params: { id: string } }) {
+  const { id } = params
+
+  try {
+    const category = await Category.findById(id)
+    return NextResponse.json({category})
+  } catch (err) {
+    console.log(err)
+    return NextResponse.json({ error: 'Category not found' }, { status: 404 })
+  }
+}
---
+++ src/app/api/category/route.ts
@@ -0,0 +1,46 @@
+import { Slug } from '@/helper/slugNormalizer'
+import database from '@/lib/database'
+import Category from '@/models/category'
+import { NextResponse, NextRequest } from 'next/server'
+
+database.connect()
+
+export const config = {
+  api: {
+    bodyParser: false,
+  }
+}
+
+export async function GET() {
+  try {
+    const categories = await Category.find()
+    return NextResponse.json(categories)
+  } catch (err) {
+    return NextResponse.json({message: 'error'})
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json()
+
+  try {
+    const found = await Category.find({ name: body.name })
+    if (found.length !== 0) return NextResponse.json({ error: 'This category already exists, select other name' })
+  } catch (err) {
+    return NextResponse.json({ error: 'unknow error' })
+  }
+
+  const newSlug = Slug.createFromText(body.name).value
+
+  try {
+    await Category.create({
+      name: body.name,
+      icon: body.icon,
+      slug: newSlug
+    })
+    return NextResponse.json({ success: true })
+  } catch (err) {
+    console.log(err)
+    return NextResponse.json({error: 'Erron saving the category.'}, { status: 400 })
+  }
+}
